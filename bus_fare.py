6e20a9a06d6e6f529eaa3931aa95ed875b014444
Manit Kumar <manit10cse@gmail.com>
2026-02-10 01:28:35 +0530

    Add files via upload
    "Initial commit :  Added bus fare calculation script".

---
+++ bus_fare.py
@@ -0,0 +1,45 @@
+# Bus fare calculation based on age and day of the week
+age = int(input("Enter the age of the passenger : " ))
+distance = float(input("Enter the distance traveled in km: "))
+# 1. Determine the base price per km based on age
+if age<=12:
+    base_price =2.5
+    fare = base_price * distance
+    print(fare)
+elif age>=60:
+    base_price = 3
+    fare = base_price * distance
+    print(fare)
+# 2. Determine the day of the week and apply discounts if applicable
+else :
+    day = input("Enter the day of the week (e.g. monday, tuesday, etc.) : ").strip().lower()
+# 3. Calculate the fare based on the day of the week using match-case statement
+    match day :
+        case "monday":
+            base_price = 5 # no discount
+            fare = base_price * distance
+            print(f" Fare for {day}   : {fare}")
+        case "tuesday":
+            base_price = 5 # no  discount
+            fare = base_price * distance
+            print(f" Fare for {day}   : {fare}")
+        case "wednesday":
+            base_price = 5 # no discount
+            fare = base_price * distance
+            print(f" Fare for {day}   : {fare}")
+        case "thursday":
+            base_price = 5 # no discount
+            fare = base_price * distance
+            print(f" Fare for {day}   : {fare}")
+        case "friday":
+            base_price = 5 # no discount
+            fare = base_price * distance
+            print(f" Fare for {day}   : {fare}")
+        case "saturday":
+            base_price = 4.5 # 10% discount
+            fare = base_price * distance 
+            print(f" Fare for {day}   : {fare}")   
+        case "sunday":
+            base_price = 4.5 # 10% discount
+            fare = base_price * distance
+            print(f" Fare for {day}   : {fare}")
